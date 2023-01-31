39e2f184dd258af60a13fd798d572796dc30467d
Christopher Doris <github.com/cjdoris>
2023-01-31 18:05:54 +0000

    adds basic read_html

--- src/PandasIO.jl
+++ src/PandasIO.jl
@@ -1,9 +1,12 @@
 module PandasIO
 
+import AbstractTrees
+import Gumbo
 import JSON3
 import Tables
 
 include("read_json.jl")
 include("to_json.jl")
+include("read_html.jl")
 
 end # module PandasIO
