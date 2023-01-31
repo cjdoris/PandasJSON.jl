433eadf85484069774ad33a2a960ee6e3c57e422
Christopher Doris <github.com/cjdoris>
2023-01-31 14:27:22 +0000

    adds to_json

--- README.md
+++ README.md
@@ -29,3 +29,24 @@ julia> df = DataFrame(PandasIO.read_json("examples/frame-01-table.json"))
    2 │     2      2.2  bar     false
    3 │     3      3.3  baz      true
 ```
+
+In the following example, we use `to_json` to write a JSON-formatted dataframe to disk,
+and show the resulting JSON. This file can be loaded into Python using `pandas.read_json`.
+
+```julia
+julia> using PandasIO, DataFrames
+
+julia> df = DataFrame(x=[1,2,3], y=[true,false,missing])
+3×2 DataFrame
+ Row │ x      y
+     │ Int64  Bool?
+─────┼────────────────
+   1 │     1     true
+   2 │     2    false
+   3 │     3  missing
+
+julia> PandasIO.to_json("example.json", df)
+
+julia> println(read("example.json", String))
+{"y":{"1":false,"0":true,"2":null},"x":{"1":2,"0":1,"2":3}}
+```
---
+++ src/to_json.jl
@@ -0,0 +1,132 @@
+"""
+    to_json(filename_or_io, table; orient=:columns)
+
+Write the given table to the given file in JSON format.
+"""
+function to_json(io::IO, table; orient::Symbol=:columns)
+    if orient === :columns
+        _to_json_columns(io, table)
+    elseif orient === :index
+        _to_json_index(io, table)
+    elseif orient === :records
+        _to_json_records(io, table)
+    elseif orient === :split
+        _to_json_split(io, table)
+    elseif orient === :table
+        _to_json_table(io, table)
+    elseif orient === :values
+        _to_json_values(io, table)
+    else
+        error("invalid orient=$(repr(orient))")
+    end
+    return
+end
+
+function to_json(filename::AbstractString, table; kw...)
+    return open(filename, "w") do io
+        to_json(io, table; kw...)
+    end
+end
+
+_to_json_item(x::Union{Nothing,Bool,Real}) = x
+_to_json_item(x::Missing) = nothing
+_to_json_item(x::Real) = isfinite(x) ? x : nothing
+_to_json_item(x::Union{AbstractVector,AbstractSet,Tuple}) = [_to_json_item(x) for x in x]
+_to_json_item(x::AbstractDict) = Dict(string(k) => _to_json_item(v) for (k, v) in x)
+
+function _to_json_columns(io, table; cols=Tables.columns(table))
+    data = Dict{Symbol,Dict{String,Any}}()
+    for colname in Tables.columnnames(cols)
+        data[colname] = Dict{String,Any}(
+            string(i-1) => _to_json_item(x)
+            for (i, x) in enumerate(Tables.getcolumn(cols, colname))
+        )
+    end
+    JSON3.write(io, data)
+end
+
+function _to_json_index(io, table; rows=Tables.rows(table), sch=Tables.schema(rows))
+    data = Dict{String,Dict{Symbol,Any}}()
+    for (i, row) in enumerate(rows)
+        newrow = Dict{Symbol,Any}()
+        Tables.eachcolumn(sch, row) do x, _, colname
+            newrow[colname] = _to_json_item(x)
+        end
+        data[string(i-1)] = newrow
+    end
+    JSON3.write(io, data)
+end
+
+function _to_json_records(io, table; rows=Tables.rows(table), sch=Tables.schema(rows))
+    data = Vector{Dict{Symbol,Any}}()
+    for row in rows
+        newrow = Dict{Symbol,Any}()
+        Tables.eachcolumn(sch, row) do x, _, colname
+            newrow[colname] = _to_json_item(x)
+        end
+        push!(data, newrow)
+    end
+    JSON3.write(io, data)
+end
+
+function _to_json_split(io, table; rows=Tables.rows(table), sch=Tables.schema(rows))
+    data = Vector{Any}[]
+    index = String[]
+    columns = collect(Tables.columnnames(rows))
+    for (i, row) in enumerate(rows)
+        push!(index, string(i-1))
+        newrow = []
+        Tables.eachcolumn(sch, row) do x, _, _
+            push!(newrow, _to_json_item(x))
+        end
+        push!(data, newrow)
+    end
+    JSON3.write(io, (; index, columns, data))
+end
+
+_to_json_field_type(::Type{<:Union{Nothing,Bool}}) = "boolean"
+_to_json_field_type(::Type{<:Union{Nothing,Integer}}) = "integer"
+_to_json_field_type(::Type{<:Union{Nothing,Real}}) = "number"
+_to_json_field_type(::Type{<:Union{Nothing,AbstractString}}) = "string"
+_to_json_field_type(::Type{T}) where {T} = "string"
+
+function _to_json_table(io, table; rows=Tables.rows(table), sch=Tables.schema(rows))
+    idxname = :index
+    while idxname in sch.names
+        idxname = Symbol("_", idxname)
+    end
+    data = Dict{Symbol,Any}[]
+    fields = [
+        (
+            name = colname,
+            type = _to_json_field_type(coltype),
+        )
+        for (colname, coltype) in zip(sch.names, sch.types)
+    ]
+    push!(fields, (name = idxname, type = "integer"))
+    for (i, row) in enumerate(rows)
+        newrow = Dict{Symbol,Any}()
+        Tables.eachcolumn(sch, row) do x, _, colname
+            newrow[colname] = _to_json_item(x)
+        end
+        newrow[idxname] = i-1
+        push!(data, newrow)
+    end
+    schema = (;
+        fields,
+        primaryKey = [idxname]
+    )
+    JSON3.write(io, (; schema, data))
+end
+
+function _to_json_values(io, table; rows=Tables.rows(table), sch=Tables.schema(rows))
+    data = Vector{Vector{Any}}()
+    for row in rows
+        newrow = []
+        Tables.eachcolumn(sch, row) do x, _, _
+            push!(newrow, _to_json_item(x))
+        end
+        push!(data, newrow)
+    end
+    JSON3.write(io, data)
+end
