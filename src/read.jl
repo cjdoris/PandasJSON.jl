e035d33695d88f44a9c34cc25be7e49d80b50d69
Christopher Doris <github.com/cjdoris>
2023-02-08 18:21:12 +0000

    rename internals

--- src/read.jl
+++ src/read.jl
@@ -1,15 +1,15 @@
-_read_json_item(::Type{T}, x) where {T} = x === nothing ? missing : convert(T, x)::T
-_read_json_item(::Type{Any}, x) = _read_json_item(x)
+_from_json(::Type{T}, x) where {T} = x === nothing ? missing : convert(T, x)::T
+_from_json(::Type{Any}, x) = _from_json(x)
 
-_read_json_item(x) =
+_from_json(x) =
     x === nothing ? missing :
-    x isa AbstractVector ? [_read_json_item(x) for x in x] :
-    x isa AbstractDict ? [_read_json_item(x) for x in x] :
+    x isa AbstractVector ? [_from_json(x) for x in x] :
+    x isa AbstractDict ? [_from_json(x) for x in x] :
     x isa Symbol ? String(x) :
     x isa AbstractString ? convert(String, x) : x
 
-function _read_json_table_column(::Type{T}, k, rows) where {T}
-    return [_read_json_item(T, get(row, k, nothing)) for row in rows]
+function _read_table_column(::Type{T}, k, rows) where {T}
+    return [_from_json(T, get(row, k, nothing)) for row in rows]
 end
 
 function _sort_index!(idx)
@@ -26,42 +26,42 @@ function _sort_columns!(cols)
     return sort!(cols)
 end
 
-function _read_json_columns!(io, data, index)
+function _read_columns!(io, data, index)
     o = JSON3.read(io, Dict{Symbol, Dict{String,Any}})
     idx = _sort_index!(unique!([k for col in values(o) for k in keys(col)]))
     index !== nothing && push!(data, index => idx)
     cols = _sort_columns!(collect(keys(o)))
     for k in cols
         col = o[k]
-        push!(data, k => [_read_json_item(get(col, i, nothing)) for i in idx])
+        push!(data, k => [_from_json(get(col, i, nothing)) for i in idx])
     end
 end
 
-function _read_json_index!(io, data, index)
+function _read_index!(io, data, index)
     o = JSON3.read(io, Dict{String,Dict{Symbol,Any}})
     idx = _sort_index!(collect(keys(o)))
     index !== nothing && push!(data, idx)
     cols = _sort_columns!(unique!([k for row in values(o) for k in keys(row)]))
     for k in cols
-        push!(data, k => [_read_json_item(get(o[i], k, nothing)) for i in idx])
+        push!(data, k => [_from_json(get(o[i], k, nothing)) for i in idx])
     end
 end
 
-function _read_json_records!(io, data, index)
+function _read_records!(io, data, index)
     o = JSON3.read(io, Vector{Dict{Symbol,Any}})
     index !== nothing && push!(data, index => 1:length(o))
     cols = _sort_columns!(unique!([k for row in o for k in keys(row)]))
     for k in cols
-        push!(data, k => [_read_json_item(get(row, k, nothing)) for row in o])
+        push!(data, k => [_from_json(get(row, k, nothing)) for row in o])
     end
 end
 
-function _read_json_split!(io, data, index)
+function _read_split!(io, data, index)
     o = JSON3.read(io, @NamedTuple{columns::Vector{Symbol}, index::Vector, data::Vector{Vector{Any}}})
     index !== nothing && push!(data, index => map(identity, o.index))
     cols = o.columns
     for (i, k) in pairs(cols)
-        push!(data, k => [_read_json_item(row[i]) for row in o.data])
+        push!(data, k => [_from_json(row[i]) for row in o.data])
     end
 end
 
@@ -80,22 +80,22 @@ function _field_type(t)
     error("unknown field type: $(repr(t))")
 end
 
-function _read_json_table!(io, data, index)
+function _read_table!(io, data, index)
     o = JSON3.read(io, @NamedTuple{schema::@NamedTuple{fields::Vector{@NamedTuple{name::Symbol,type::Any}}, primaryKey::Vector{Symbol}}, data::Vector{Dict{Symbol,Any}}})
     for fld in o.schema.fields
         k = fld.name
         index === nothing && k in o.schema.primaryKey && continue
         T = _field_type(fld.type)
-        push!(data, k => _read_json_table_column(T, k, o.data))
+        push!(data, k => _read_table_column(T, k, o.data))
     end
 end
 
-function _read_json_values!(io, data, index)
+function _read_values!(io, data, index)
     o = JSON3.read(io, Vector{Vector{Any}})
     index !== nothing && push!(data, index => 1:length(o))
     ncols = length(first(o))
     for k in 1:ncols
-        push!(data, Symbol("Column$k") => [_read_json_item(row[k]) for row in o])
+        push!(data, Symbol("Column$k") => [_from_json(row[k]) for row in o])
     end
 end
 
@@ -125,17 +125,17 @@ function read(io::IO; orient::AbstractString="columns", index::Union{Nothing,Sym
     # parse into columnname => columndata pairs
     data = Vector{Pair{Symbol,Vector}}()
     if orient == "columns"
-        _read_json_columns!(io, data, index)
+        _read_columns!(io, data, index)
     elseif orient == "index"
-        _read_json_index!(io, data, index)
+        _read_index!(io, data, index)
     elseif orient == "records"
-        _read_json_records!(io, data, index)
+        _read_records!(io, data, index)
     elseif orient == "split"
-        _read_json_split!(io, data, index)
+        _read_split!(io, data, index)
     elseif orient == "table"
-        _read_json_table!(io, data, index)
+        _read_table!(io, data, index)
     elseif orient == "values"
-        _read_json_values!(io, data, index)
+        _read_values!(io, data, index)
     else
         error("invalid orient=$(repr(orient))")
     end
--- src/write.jl
+++ src/write.jl
@@ -22,17 +22,17 @@ function write(io::IO, table; orient::AbstractString="columns", index::Union{Boo
         error("index=false only supported for orient=\"split\" or orient=\"table\"")
     end
     if orient == "columns"
-        _to_json_columns(io, table; index)
+        _write_columns(io, table; index)
     elseif orient == "index"
-        _to_json_index(io, table; index)
+        _write_index(io, table; index)
     elseif orient == "records"
-        _to_json_records(io, table)
+        _write_records(io, table)
     elseif orient == "split"
-        _to_json_split(io, table; index)
+        _write_split(io, table; index)
     elseif orient == "table"
-        _to_json_table(io, table; index)
+        _write_table(io, table; index)
     elseif orient == "values"
-        _to_json_values(io, table)
+        _write_values(io, table)
     else
         error("invalid orient=$(repr(orient))")
     end
@@ -41,54 +41,54 @@ end
 
 write(filename::AbstractString, table; kw...) = open(io->write(io, table; kw...), filename, "w")
 
-_to_json_item(x::Union{Nothing,Bool,Real,AbstractString}) = x
-_to_json_item(x::Missing) = nothing
-_to_json_item(x::Real) = isfinite(x) ? x : nothing
-_to_json_item(x::Union{AbstractVector,AbstractSet,Tuple}) = [_to_json_item(x) for x in x]
-_to_json_item(x::AbstractDict) = Dict(string(k) => _to_json_item(v) for (k, v) in x)
+_to_json(x::Union{Nothing,Bool,Real,AbstractString}) = x
+_to_json(x::Missing) = nothing
+_to_json(x::Real) = isfinite(x) ? x : nothing
+_to_json(x::Union{AbstractVector,AbstractSet,Tuple}) = [_to_json(x) for x in x]
+_to_json(x::AbstractDict) = Dict(string(k) => _to_json(v) for (k, v) in x)
 
-function _to_json_columns(io, table; cols=Tables.columns(table), index)
+function _write_columns(io, table; cols=Tables.columns(table), index)
     data = Dict{Symbol,Dict{String,Any}}()
     for colname in Tables.columnnames(cols)
         data[colname] = Dict{String,Any}(
-            string(index isa Bool ? i-1 : index[begin+i-1]) => _to_json_item(x)
+            string(index isa Bool ? i-1 : index[begin+i-1]) => _to_json(x)
             for (i, x) in enumerate(Tables.getcolumn(cols, colname))
         )
     end
     JSON3.write(io, data)
 end
 
-function _to_json_index(io, table; rows=Tables.rows(table), sch=Tables.schema(rows), index)
+function _write_index(io, table; rows=Tables.rows(table), sch=Tables.schema(rows), index)
     data = Dict{String,Dict{Symbol,Any}}()
     for (i, row) in enumerate(rows)
         newrow = Dict{Symbol,Any}()
         Tables.eachcolumn(sch, row) do x, _, colname
-            newrow[colname] = _to_json_item(x)
+            newrow[colname] = _to_json(x)
         end
         data[string(index isa Bool ? i-1 : index[begin+i-1])] = newrow
     end
     JSON3.write(io, data)
 end
 
-function _to_json_records(io, table; rows=Tables.rows(table), sch=Tables.schema(rows))
+function _write_records(io, table; rows=Tables.rows(table), sch=Tables.schema(rows))
     data = Vector{Dict{Symbol,Any}}()
     for row in rows
         newrow = Dict{Symbol,Any}()
         Tables.eachcolumn(sch, row) do x, _, colname
-            newrow[colname] = _to_json_item(x)
+            newrow[colname] = _to_json(x)
         end
         push!(data, newrow)
     end
     JSON3.write(io, data)
 end
 
-function _to_json_split(io, table; rows=Tables.rows(table), sch=Tables.schema(rows), index)
+function _write_split(io, table; rows=Tables.rows(table), sch=Tables.schema(rows), index)
     data = Vector{Any}[]
     columns = collect(Tables.columnnames(rows))
     for row in rows
         newrow = []
         Tables.eachcolumn(sch, row) do x, _, _
-            push!(newrow, _to_json_item(x))
+            push!(newrow, _to_json(x))
         end
         push!(data, newrow)
     end
@@ -98,7 +98,7 @@ function _to_json_split(io, table; rows=Tables.rows(table), sch=Tables.schema(ro
     else
         include_index = true
         length(index) == length(data) || error("index must be the same length as the table")
-        index = [_to_json_item(x) for x in index]
+        index = [_to_json(x) for x in index]
     end
     if include_index
         JSON3.write(io, (; index, columns, data))
@@ -130,7 +130,7 @@ function _table_schema_field(name, T)
     return NamedTuple{(:name,:type,:extDtype),Tuple{Symbol,String,Union{String,Nothing}}}((n,t,e))
 end
 
-function _to_json_table(io, table; rows=Tables.rows(table), sch=Tables.schema(rows), index)
+function _write_table(io, table; rows=Tables.rows(table), sch=Tables.schema(rows), index)
     include_index = !isa(index, Bool) || index
     idxname = :index
     while idxname in sch.names
@@ -147,7 +147,7 @@ function _to_json_table(io, table; rows=Tables.rows(table), sch=Tables.schema(ro
     for (i, row) in enumerate(rows)
         newrow = Dict{Symbol,Any}()
         Tables.eachcolumn(sch, row) do x, _, colname
-            newrow[colname] = _to_json_item(x)
+            newrow[colname] = _to_json(x)
         end
         if include_index
             newrow[idxname] = index isa Bool ? i-1 : index[begin+i-1]
@@ -163,12 +163,12 @@ function _to_json_table(io, table; rows=Tables.rows(table), sch=Tables.schema(ro
     end
 end
 
-function _to_json_values(io, table; rows=Tables.rows(table), sch=Tables.schema(rows))
+function _write_values(io, table; rows=Tables.rows(table), sch=Tables.schema(rows))
     data = Vector{Vector{Any}}()
     for row in rows
         newrow = []
         Tables.eachcolumn(sch, row) do x, _, _
-            push!(newrow, _to_json_item(x))
+            push!(newrow, _to_json(x))
         end
         push!(data, newrow)
     end
